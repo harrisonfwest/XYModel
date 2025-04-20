e9921248ac0195a5ef0141a7add9fa73f70a4088
HwestCode <hwest@haverford.edu>
2025-04-20 16:30:04 -0400

    Update XYModel.py

--- XYModel.py
+++ XYModel.py
@@ -5,3 +5,13 @@ Created on Sun Apr 20, 2025 @ 4:09 pm
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+def newLattice1D(width: int) -> np.ndarray:
+    lattice = np.random.uniform(-np.pi, np.pi, size=width)
+    return lattice
+
+def energyof1D(lattice: np.ndarray) -> float:
+    energy = 0
+    for i in range(len(lattice)):
+        energy += np.cos(lattice[i] - lattice[(i+1)%len(lattice)])
+    return energy
