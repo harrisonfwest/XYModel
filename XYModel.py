97545baaad1b9d9188c5845f21826d634c6714db
HwestCode <hwest@haverford.edu>
2025-04-20 19:12:40 -0400

    Update XYModel.py

--- XYModel.py
+++ XYModel.py
@@ -11,7 +11,9 @@ def newLattice1D(width: int) -> np.ndarray:
     return lattice
 
 def showLattice(lattice: np.ndarray) -> None:
-    plt.imshow(lattice, cmap = 'cool')
+    plt.imshow(lattice, cmap = 'hsv', vmin = -np.pi, vmax = +np.pi)
+    plt.ylim(0, len(lattice[0]))
+    plt.xlim(0, len(lattice[0]))
     plt.colorbar(ticks = [-np.pi+.1, 0, np.pi-.1]).ax.set_yticklabels(['-$\pi$', 0, '$\pi$'])
     plt.show()
 
@@ -27,4 +29,32 @@ def squareLattice2D(width: int) -> np.ndarray:
     lattice = np.random.uniform(-np.pi, np.pi, size=[width, width])
     return lattice
 
-showLattice(squareLattice2D(100))
+def energyof2D(lattice: np.ndarray) -> float:
+    energy = 0
+    for i in range(len(lattice)):
+        for j in range(len(lattice)):
+            energy += np.cos(lattice[i, j] - lattice[(i+1)%len(lattice[0]), (j+1)%len(lattice[0])])
+    return energy
+
+def equilibrate2D(lattice: np.ndarray, temp: float) -> np.ndarray:
+    width = len(lattice[0])
+    for _ in range(20000):
+        i = np.random.choice(width)
+        j = np.random.choice(width)
+        oldE = energyof2D(lattice)
+        newLattice = lattice
+        for k in np.linspace(2*np.pi, 0):
+            newL
+        if newLattice[i, j] + 0.5 > np.pi:
+            newLattice[i, j] = newLattice[i, j] - (np.pi - 0.5)
+        else:
+            newLattice[i, j] + 0.5
+        newE = energyof2D(newLattice)
+        if newE < oldE or np.random.random() < np.exp(-newE/temp):
+            lattice = newLattice
+    return lattice
+
+sample = squareLattice2D(50)
+showLattice(sample)
+sample = equilibrate2D(sample, 1.5)
+showLattice(sample)
