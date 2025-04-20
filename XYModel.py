46d5db2fa2320d6d485fc5d100e0e06e475098ad
HwestCode <hwest@haverford.edu>
2025-04-20 18:51:38 -0400

    Update XYModel.py

--- XYModel.py
+++ XYModel.py
@@ -10,8 +10,21 @@ def newLattice1D(width: int) -> np.ndarray:
     lattice = np.random.uniform(-np.pi, np.pi, size=width)
     return lattice
 
+def showLattice(lattice: np.ndarray) -> None:
+    plt.imshow(lattice, cmap = 'cool')
+    plt.colorbar(ticks = [-np.pi+.1, 0, np.pi-.1]).ax.set_yticklabels(['-$\pi$', 0, '$\pi$'])
+    plt.show()
+
+# showLattice(newLattice1D(10))
+
 def energyof1D(lattice: np.ndarray) -> float:
     energy = 0
     for i in range(len(lattice)):
         energy += np.cos(lattice[i] - lattice[(i+1)%len(lattice)])
     return energy
+
+def squareLattice2D(width: int) -> np.ndarray:
+    lattice = np.random.uniform(-np.pi, np.pi, size=[width, width])
+    return lattice
+
+showLattice(squareLattice2D(100))
