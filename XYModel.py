f9098c7e6b6e4ada9859a79b408097b47200bdd2
HwestCode <hwest@haverford.edu>
2025-04-21 01:31:27 -0400

    Update XYModel.py

--- XYModel.py
+++ XYModel.py
@@ -6,53 +6,47 @@ Created on Sun Apr 20, 2025 @ 4:09 pm
 import numpy as np
 import matplotlib.pyplot as plt
 
-def newLattice1D(width: int) -> np.ndarray:
-    lattice = np.random.uniform(-np.pi, np.pi, size=width)
-    return lattice
+class lattice():
+    def __init__(self, temperature = .1, width = 15):
+        self.width = width
+        self.size = self.width * self.width
+        L, N = self.width, self.size
+        self.neighbors = {i : ((i//L)*L + (i+1)%L, (i+L)%N, (i-1)%L, (i//L)*L + (i-L)%N) for i in list(range(N))}
+        self.spins = np.random.uniform(-np.pi, np.pi, self.size)
+        self.temperature = temperature
+        self.energy = np.sum(self.get_energy())/N
 
-def showLattice(lattice: np.ndarray) -> None:
-    plt.imshow(lattice, cmap = 'twilight_shifted', vmin = -np.pi, vmax = +np.pi)
-    plt.ylim(0, len(lattice[0]))
-    plt.xlim(0, len(lattice[0]))
-    plt.colorbar(ticks = [-np.pi+.1, 0, np.pi-.1]).ax.set_yticklabels(['-$\pi$', 0, '$\pi$'])
-    plt.show()
+    def set_temperature(self, temperature):
+        self.temperature = temperature
 
-# showLattice(newLattice1D(10))
+    def poke(self): # tries a random spin for each lattice site in a random order
+        beta = 1 / self.temperature
+        sites = list(range(len(self.spins)))
+        np.random.shuffle(sites)
+        for site in sites:
+            oldEnergy = -np.sum(np.cos(self.spins[site] - self.spins[n]) for n in self.neighbors[site])
+            newSpin = self.spins[site] + np.random.uniform(-np.pi, np.pi)
+            if newSpin > 2*np.pi:
+                newSpin -= 2*np.pi
+            newEnergy = -np.sum(np.cos(newSpin - self.spins[n]) for n in self.neighbors[site])
+            if np.random.rand() < np.exp(-(newEnergy - oldEnergy) * beta):
+                self.spins[site] = newSpin
 
-def energyof1D(lattice: np.ndarray) -> float:
-    energy = 0
-    for i in range(len(lattice)):
-        energy += np.cos(lattice[i] - lattice[(i+1)%len(lattice)])
-    return energy
+    def get_energy(self):
+        energy = np.zeros(np.shape(self.spins))
+        site = 0
+        for spin in self.spins:
+            energy[site] = -sum(np.cos(spin - self.spins[n]) for n in self.neighbors[site])
+            site += 1
+        return energy
 
-def squareLattice2D(width: int) -> np.ndarray:
-    lattice = np.random.uniform(-np.pi, np.pi, size=[width, width])
-    return lattice
+    def plot_lattice(self):
+        grid = self.spins.reshape(self.width, self.width)
+        plt.imshow(grid, cmap = 'twilight_shifted', vmin = -np.pi, vmax = +np.pi)
+        plt.colorbar()
+        plt.colorbar(ticks=[-np.pi + .1, 0, np.pi - .1]).ax.set_yticklabels(['-$\pi$', 0, '$\pi$'])
+        plt.show()
 
-def energyof2D(lattice: np.ndarray) -> float:
-    energy = 0
-    for i in range(len(lattice)):
-        for j in range(len(lattice)):
-            energy -= (np.cos(lattice[i, j] - lattice[(i+1)%len(lattice[0]), j])
-            + np.cos(lattice[i, j] - lattice[i, (j+1)%len(lattice[0])]))
-    return energy/(2 * len(lattice)**2)
-
-def equilibrate2D(lattice: np.ndarray, temp: float) -> np.ndarray:
-    currLattice = lattice
-    width = len(lattice[0])
-    for k in range(len(lattice)):
-        i = np.random.choice(range(width))
-        j = np.random.choice(range(width))
-        oldE = energyof2D(currLattice)
-        newLattice = np.copy(currLattice)
-        newLattice[i][j] = np.random.uniform(-np.pi, np.pi)
-        newE = energyof2D(newLattice)
-        if np.random.uniform(0, 1) < np.exp(-(newE - oldE)/temp):
-            currLattice[i][j] = newLattice[i][j]
-    return currLattice
-
-sample = squareLattice2D(512)
-showLattice(sample)
+sample = lattice(width = 256)
 while True:
-    sample = equilibrate2D(sample, 0.005 )
-    showLattice(sample)
+    sample.plot_lattice()
