44f75f503141fd117220e8e0d63974d21c942d5e
HwestCode <hwest@haverford.edu>
2025-04-21 01:43:12 -0400

    Update XYModel.py

--- XYModel.py
+++ XYModel.py
@@ -11,7 +11,7 @@ class lattice():
         self.width = width
         self.size = self.width * self.width
         L, N = self.width, self.size
-        self.neighbors = {i : ((i//L)*L + (i+1)%L, (i+L)%N, (i-1)%L, (i//L)*L + (i-L)%N) for i in list(range(N))}
+        self.neighbors = {i : ((i//L)*L + (i+1)%L, (i+L)%N, (i//L)*L + (i-L)%L, (i-L)%N) for i in list(range(N))}
         self.spins = np.random.uniform(-np.pi, np.pi, self.size)
         self.temperature = temperature
         self.energy = np.sum(self.get_energy())/N
@@ -24,29 +24,28 @@ class lattice():
         sites = list(range(len(self.spins)))
         np.random.shuffle(sites)
         for site in sites:
-            oldEnergy = -np.sum(np.cos(self.spins[site] - self.spins[n]) for n in self.neighbors[site])
+            oldEnergy = -sum(np.cos(self.spins[site] - self.spins[n]) for n in self.neighbors[site])
             newSpin = self.spins[site] + np.random.uniform(-np.pi, np.pi)
             if newSpin > 2*np.pi:
                 newSpin -= 2*np.pi
-            newEnergy = -np.sum(np.cos(newSpin - self.spins[n]) for n in self.neighbors[site])
+            newEnergy = -sum(np.cos(newSpin - self.spins[n]) for n in self.neighbors[site])
             if np.random.rand() < np.exp(-(newEnergy - oldEnergy) * beta):
                 self.spins[site] = newSpin
 
     def get_energy(self):
         energy = np.zeros(np.shape(self.spins))
-        site = 0
-        for spin in self.spins:
-            energy[site] = -sum(np.cos(spin - self.spins[n]) for n in self.neighbors[site])
-            site += 1
+        for site in range(len(self.spins)):
+            energy[site] = -sum(np.cos(self.spins[site] - self.spins[n]) for n in self.neighbors[site])
         return energy
 
     def plot_lattice(self):
         grid = self.spins.reshape(self.width, self.width)
         plt.imshow(grid, cmap = 'twilight_shifted', vmin = -np.pi, vmax = +np.pi)
-        plt.colorbar()
         plt.colorbar(ticks=[-np.pi + .1, 0, np.pi - .1]).ax.set_yticklabels(['-$\pi$', 0, '$\pi$'])
+        plt.ylim(0, self.width)
         plt.show()
 
 sample = lattice(width = 256)
 while True:
     sample.plot_lattice()
+    sample.poke()
