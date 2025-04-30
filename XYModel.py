439f5f9d92b8bad26f491b240a1680c34f190abf
HwestCode <hwest@haverford.edu>
2025-04-30 16:47:16 -0400

    revert

--- XYModel.py
+++ XYModel.py
@@ -15,40 +15,40 @@ class lattice():
         self.size = self.width * self.width
         self.h = external_field
         L, N = self.width, self.size
-        # Neighbors in order:               Right,  Bottom,               Left,      Up
-        self.neighbors = {i : ((i//L)*L + (i+1)%L, (i+L)%N, (i//L)*L + (i-1)%L, (i-L)%N) for i in list(range(N))}
-        self.spins = np.random.uniform(0, 2*np.pi, self.size)
+        self.neighbors = {i : ((i//L)*L + (i+1)%L, (i+L)%N, (i//L)*L + (i-L)%L, (i-L)%N) for i in list(range(N))}
+        self.spins = np.random.uniform(-np.pi, np.pi, self.size)
         self.temperature = temperature
-        self.energy = np.sum(self.get_energy())/N  # Energy per spin is total energy / number of spins
+        self.energy = np.sum(self.get_energy())/N
         self.fig = plt.figure()
         self.im = plt.imshow(self.spins.reshape(self.width, self.width), cmap = 'twilight_shifted',
-                             vmin = 0, vmax = 2*np.pi)
+                             vmin = -np.pi, vmax = +np.pi)
+        plt.colorbar(ticks=[-np.pi + .1, 0, np.pi - .1]).ax.set_yticklabels(['-$\pi$', 0, '$\pi$'])
+        plt.axis('off')
 
     def poke(self): # tries a random new spin for each lattice site, in a random order of sites
         beta = 1 / self.temperature
         sites = list(range(len(self.spins)))
         np.random.shuffle(sites)
-        for site in sites: # site is an index in the lattice; Check each one
-            oldEnergy = -sum(np.cos(self.spins[site] - self.spins[n]) for n in self.neighbors[site]) \
-                        - (self.h * np.cos(self.spins[site]))
-            newSpin = self.spins[site] + np.random.uniform(0, 2*np.pi)
-            if newSpin >= 2*np.pi:
+        for site in sites:
+            oldEnergy = -sum(np.cos(self.spins[site] - self.spins[n]) for n in self.neighbors[site]) - (self.h * np.cos(self.spins[site]))
+            newSpin = self.spins[site] + np.random.uniform(-np.pi, np.pi)
+            if newSpin >= np.pi:
                 newSpin -= 2*np.pi
-            newEnergy = -sum(np.cos(newSpin - self.spins[n]) for n in self.neighbors[site]) \
-                        - self.h * np.cos(self.spins[site])
-            if newEnergy <= oldEnergy or np.random.rand() < np.exp(-(newEnergy - oldEnergy) * beta):
+            newEnergy = -sum(np.cos(newSpin - self.spins[n]) for n in self.neighbors[site]) - (self.h * np.cos(self.spins[site]))
+            if np.random.rand() < np.exp(-(newEnergy - oldEnergy) * beta):
                 self.spins[site] = newSpin
 
     def get_energy(self):
         energy = np.zeros(np.shape(self.spins))
         for site in range(len(self.spins)):
-            energy[site] = - sum(np.cos(self.spins[site] - self.spins[n]) for n in self.neighbors[site]) \
-                           - self.h * np.cos(self.spins[site])
-        # returns array of energy at each site, summed to get total energy, and then divided by number of spins
-        # to get average energy per site of system
+            energy[site] = -sum(np.cos(self.spins[site] - self.spins[n]) for n in self.neighbors[site]) - (self.h * np.cos(self.spins[site]))
         return energy
 
     def animate(self):
+        grid = self.spins.reshape(self.width, self.width)
+        self.im = plt.imshow(grid, cmap = 'twilight_shifted', vmin = -np.pi, vmax = +np.pi)
+
+    def animate(self, frame):
         for _ in range(15):
             self.poke()
         grid = self.spins.reshape(self.width, self.width)
@@ -77,13 +77,11 @@ class lattice():
 
     def show(self):
         grid = self.spins.reshape(self.width, self.width)
-        plot = plt.imshow(grid, cmap = 'twilight', vmin = 0, vmax = 2*np.pi)
-        plt.colorbar(plot, label = '$\heta$').ax.set_yticks([0, np.pi, 2* np.pi], ['0', '$\pi$', '2$\pi$'])
+        plt.imshow(grid, cmap = 'twilight_shifted', vmin = -np.pi, vmax = +np.pi)
+        plt.colorbar(ticks=[-np.pi + .1, 0, np.pi - .1]).ax.set_yticklabels(['-$\pi$', 0, '$\pi$'])
         plt.axis('off')
         plt.show()
 
-sample = lattice(width = 128)
-for _ in range(100):
-    sample.show()
-    sample.poke()
-sample.show()
+sample = lattice(width = 512)
+sample.make_animation()
+# sample.make_animation(prepend = 'lattice')
