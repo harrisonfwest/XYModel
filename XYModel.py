e8509114fa2d09315a5a9f5d09e7cc8bc6341558
HwestCode <hwest@haverford.edu>
2025-04-30 17:02:35 -0400

    update

--- XYModel.py
+++ XYModel.py
@@ -15,14 +15,13 @@ class lattice():
         self.size = self.width * self.width
         self.h = external_field
         L, N = self.width, self.size
-        self.neighbors = {i : ((i//L)*L + (i+1)%L, (i+L)%N, (i//L)*L + (i-L)%L, (i-L)%N) for i in list(range(N))}
-        self.spins = np.random.uniform(-np.pi, np.pi, self.size)
+        self.neighbors = {i : ((i//L)*L + (i+1)%L, (i+L)%N, (i//L)*L + (i-1)%L, (i-L)%N) for i in list(range(N))}
+        self.spins = np.random.uniform(0, 2*np.pi, self.size)
         self.temperature = temperature
-        self.energy = np.sum(self.get_energy())/N
         self.fig = plt.figure()
         self.im = plt.imshow(self.spins.reshape(self.width, self.width), cmap = 'twilight_shifted',
-                             vmin = -np.pi, vmax = +np.pi)
-        plt.colorbar(ticks=[-np.pi + .1, 0, np.pi - .1]).ax.set_yticklabels(['-$\pi$', 0, '$\pi$'])
+                             vmin = 0, vmax = 2*np.pi)
+        plt.colorbar(ticks=[0, np.pi, 2*np.pi]).ax.set_yticklabels([0, '$\pi$', '2$\pi$'])
         plt.axis('off')
 
     def poke(self): # tries a random new spin for each lattice site, in a random order of sites
@@ -32,21 +31,24 @@ class lattice():
         for site in sites:
             oldEnergy = -sum(np.cos(self.spins[site] - self.spins[n]) for n in self.neighbors[site]) - (self.h * np.cos(self.spins[site]))
             newSpin = self.spins[site] + np.random.uniform(-np.pi, np.pi)
-            if newSpin >= np.pi:
+            if newSpin >= 2*np.pi:
                 newSpin -= 2*np.pi
             newEnergy = -sum(np.cos(newSpin - self.spins[n]) for n in self.neighbors[site]) - (self.h * np.cos(self.spins[site]))
-            if np.random.rand() < np.exp(-(newEnergy - oldEnergy) * beta):
+            if newEnergy <= oldEnergy or np.random.rand() < np.exp(-(newEnergy - oldEnergy) * beta):
                 self.spins[site] = newSpin
 
     def get_energy(self):
         energy = np.zeros(np.shape(self.spins))
         for site in range(len(self.spins)):
-            energy[site] = -sum(np.cos(self.spins[site] - self.spins[n]) for n in self.neighbors[site]) - (self.h * np.cos(self.spins[site]))
+            energy[site] = -sum(np.cos(self.spins[site] - self.spins[n]) for n in self.neighbors[site]) \
+                           - self.h * np.cos(self.spins[site])
         return energy
 
+    '''
     def animate(self):
         grid = self.spins.reshape(self.width, self.width)
         self.im = plt.imshow(grid, cmap = 'twilight_shifted', vmin = -np.pi, vmax = +np.pi)
+    '''
 
     def animate(self, frame):
         for _ in range(15):
@@ -55,30 +57,31 @@ class lattice():
         self.im.set_data(grid)
         return self.im
 
-    def make_animation(self, duration = 8, prepend = 'lattice'):
-        anim = animation.FuncAnimation(self.fig, self.animate, frames = duration * 25, interval = 40)
-        video = anim.to_html5_video()
+    def make_animation(self, prepend = 'lattice'):
+        anim = animation.FuncAnimation(self.fig, self.animate)
 
-        # embedding for the video
-        html = display.HTML(video)
-
-        # draw the animation
-        display.display(html)
-        plt.close()
-
-        return
+        # video = anim.to_html5_video()
+        #
+        # # embedding for the video
+        # html = display.HTML(video)
+        #
+        # # draw the animation
+        # display.display(html)
 
         name = prepend + '.gif'
         count = 0
         while os.path.exists(name):
             count += 1
             name = prepend + str(count) + '.gif'
-        anim.save(name, writer = 'PillowWriter')
+
+        anim.save(name)
+
+        return
 
     def show(self):
         grid = self.spins.reshape(self.width, self.width)
-        plt.imshow(grid, cmap = 'twilight_shifted', vmin = -np.pi, vmax = +np.pi)
-        plt.colorbar(ticks=[-np.pi + .1, 0, np.pi - .1]).ax.set_yticklabels(['-$\pi$', 0, '$\pi$'])
+        plt.imshow(grid, cmap = 'twilight_shifted', vmin = 0, vmax = 2*np.pi)
+        # plt.colorbar(ticks=[-np.pi + .1, 0, np.pi - .1]).ax.set_yticklabels(['-$\pi$', 0, '$\pi$'])
         plt.axis('off')
         plt.show()
 
